660b9eb7ebc40f6b897c687cfca7670cbb8555b6
rlotun <rlotun@gmail.com>
2010-06-17 16:12:10 +0100

    Initial import of files.

---
+++ README.md
@@ -0,0 +1,29 @@
+Twisted WebSocket Server
+========================
+
+To run:
+
+    $ sudo python test_server.py
+
+In your browser go to http://localhost:8080.
+
+Notes
+=====
+
+This code is based off the associated branch for
+http://twistedmatrix.com/trac/ticket/4173, and includes support for the new
+`"hixie-76"` handshake (http://www.whatwg.org/specs/web-socket-protocol/) which
+is the latest draft as of June 17, 2010.
+
+The difference between this and what may actually be included in Twisted is
+that this version contains definite backwards-support for `hixie-75`, and will
+track the latest standard as fast as I can implement it. That means that this
+server should work with Chrome 5, Safari 5 and the latest development version
+of Chrome 6.
+
+If using browsers that do *not* support WebSockets, consider using a fallabck
+implementation to Flash, as seen in http://github.com/gimite/web-socket-js. The
+bundled `test_server.py` will also start a simple Flash Socket Policy server
+(see http://www.adobe.com/devnet/flashplayer/articles/socket_policy_files.html)
+and should be immediately usable with `web-socket-js`.
+
---
+++ test_server.py
@@ -0,0 +1,68 @@
+""" WebSocket test resource.
+
+This code will run a websocket resource on 8080 and reachable at ws://localhost:8080/test.
+For compatibility with web-socket-js (a fallback to Flash for browsers that do not yet support
+WebSockets) a policy server will also start on port 843.
+See: http://github.com/gimite/web-socket-js
+"""
+
+__author__ = 'Reza Lotun'
+
+
+from datetime import datetime
+
+from twisted.internet.protocol import Protocol, Factory
+from twisted.web import resource
+from twisted.web.static import File
+from twisted.internet import task
+
+from websocket import WebSocketHandler, WebSocketSite
+
+
+class Testhandler(WebSocketHandler):
+    def __init__(self, transport):
+        WebSocketHandler.__init__(self, transport)
+        self.periodic_call = task.LoopingCall(self.send_time)
+
+    def send_time(self):
+        # send current time as an ISO8601 string
+        data = datetime.utcnow().isoformat().encode('utf8')
+        self.transport.write(data)
+
+    def frameReceived(self, frame):
+        self.transport.write(frame)
+        self.periodic_call.start(0.5)
+
+    def connectionLost(self, reason):
+        print 'Lost connection: ', reason
+
+
+class FlashSocketPolicy(Protocol):
+    """ A simple Flash socket policy server.
+    See: http://www.adobe.com/devnet/flashplayer/articles/socket_policy_files.html
+    """
+    def connectionMade(self):
+        policy = '<?xml version="1.0"?><!DOCTYPE cross-domain-policy SYSTEM ' \
+                 '"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">' \
+                 '<cross-domain-policy><allow-access-from domain="*" to-ports="*" /></cross-domain-policy>'
+        self.transport.write(policy)
+        self.transport.loseConnection()
+
+
+if __name__ == "__main__":
+    from twisted.internet import reactor
+
+    # run our websocket server
+    # serve index.html from the local directory
+    root = File('.')
+    site = WebSocketSite(root)
+    site.addHandler('/test', Testhandler)
+    reactor.listenTCP(8080, site)
+
+    # run policy file server
+    factory = Factory()
+    factory.protocol = FlashSocketPolicy
+    reactor.listenTCP(843, factory)
+
+    reactor.run()
+
